2010eb3d716e09188f738231f3e6536daf2d415e
ashokkumar2342 <ashokkumar2342@gmail.com>
2025-04-12 17:17:56 +0530

    Update MyFuncs.php

--- app/Helper/MyFuncs.php
+++ app/Helper/MyFuncs.php
@@ -63,13 +63,13 @@ class MyFuncs {
 
     // $rs_fetch = DB::select(DB::raw("SELECT `web_url` from `schoolinfo` limit 1;"));
     // $web_url = $rs_fetch[0]->web_url;
-    // $web_url = "menage.eageskool.com";
+    $web_url = "menage.eageskool.com";
 
-    // $http_host =  $_SERVER['HTTP_HOST'];
-    // if(($http_host != $web_url) && ($http_host != 'localhost:80') && ($http_host != 'localhost:81') && ($http_host != 'localhost')){
-    //   return false;
-    //   return Redirect::route('logout')->with(['error_msg' => 'Unauthorised Access to Application !!']);
-    // }
+    $http_host =  $_SERVER['HTTP_HOST'];
+    if(($http_host != $web_url) && ($http_host != 'localhost:80') && ($http_host != 'localhost:81') && ($http_host != 'localhost')){
+      return false;
+      return Redirect::route('logout')->with(['error_msg' => 'Unauthorised Access to Application !!']);
+    }
 
     $user_role = $user_rs->role_id;
     $user_id = $user_rs->id;
@@ -350,7 +350,7 @@ class MyFuncs {
       // return false;  
     }
     $http_host =  $_SERVER['HTTP_HOST'];
-    if(($http_host != '10.145.121.38') && ($http_host != 'localhost') && ($http_host != 'localhost:81')){
+    if(($http_host != 'menage.eageskool.com') && ($http_host != 'localhost') && ($http_host != 'localhost:81')){
       return false;
       return Redirect::route('logout')->with(['error_msg' => 'Unauthorised Access to Application !!']);
     }
