16f2ed887996cda0beeebc9d458186a44f26a04b
ashokkumar2342 <ashokkumar2342@gmail.com>
2025-04-13 20:04:31 +0530

    Update OnlinePaymentController.php

--- app/Http/Controllers/Admin/OnlinePaymentController.php
+++ app/Http/Controllers/Admin/OnlinePaymentController.php
@@ -17,10 +17,13 @@ class OnlinePaymentController extends Controller
     public function payment(Request $request, $param_values)
     {
         try {
+
             $len_variable = intval(substr($param_values, 0, 2));
             $param_values = substr($param_values, 2);
             $school_code = substr($param_values, 0, $len_variable);
             $param_values = substr($param_values, $len_variable);
+
+            $send_param_values = $param_values;
 
             $len_variable = intval(substr($param_values, 0, 2));
             $param_values = substr($param_values, 2);
@@ -32,10 +35,10 @@ class OnlinePaymentController extends Controller
 
             $upto_year = intval(substr($param_values, 0, 4));
 
-            $dob_year = intval(substr($param_values, 4, 4));
+            $dob_year = substr($param_values, 4, 4);
             $dob_month = substr($param_values, 8, 2);
             $dob_day = substr($param_values, 10, 2);
-            $dob_complete = substr($dob_year.'-'.$dob_month.'-'.$dob_day, 0, 10);
+            $dob_complete = substr($dob_day.'-'.$dob_month.'-'.$dob_year, 0, 10);
 
             $from_ip = MyFuncs::getIp();
 
@@ -45,9 +48,10 @@ class OnlinePaymentController extends Controller
             }
             
             $final_url = $status[2].$status[3];
+            $final_url = $status[2].$status[3].'/'.$send_param_values;
             return redirect()->away($final_url);
              
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             $e_method = "payment";
             // return MyFuncs::Exception_error_handler($this->e_controller, $e_method, $e->getMessage());
         }
@@ -67,18 +71,18 @@ class OnlinePaymentController extends Controller
         }
         $rs_fetch = DB::select(DB::raw("SELECT `url`, `online_fee_url` from `admin_school_information` where `code` = '$code' limit 1;"));
         if(count($rs_fetch) == 0){
-            goto return_end;
             $status[1] = 'Something Went Wrong-01';
+            goto return_end;
         }
         if(($upto_month == 0) || ($upto_year == 0)){
+            $status[1] = 'Something Went Wrong-02';
             goto return_end;
-            $status[1] = 'Something Went Wrong-02';    
         }
 
         $temp_date = MyFuncs::check_valid_date($dob);
         if($temp_date[0] == 0){
-            goto return_end;
             $status[1] = 'Something Went Wrong-03';
+            goto return_end;
         }
         
         $status[2] = $rs_fetch[0]->url;
@@ -88,6 +92,6 @@ class OnlinePaymentController extends Controller
         return_end:
             $rs_insert = DB::select(DB::raw("INSERT into `log_hit_online_fee_url`(`from_ip`, `code`, `regis_no`, `upto_month`, `upto_year`, `dob`, `log_time`, `status`) values ('$from_ip', '$code', '$regis_no', $upto_month, $upto_year, '$dob', now(), $status[0]);"));
 
-        return $status;
+            return $status;
     }
 }
