e4c4f7069f94177544dd308dbe3684c76ab6c644
ashokkumar2342 <ashokkumar2342@gmail.com>
2025-06-21 16:01:23 +0530

    Update ApiController.php

--- app/Http/Controllers/Admin/ApiController.php
+++ app/Http/Controllers/Admin/ApiController.php
@@ -111,10 +111,8 @@ public function strem_video(Request $request)
         $rec_id = Crypt::decrypt($request->id);
         $token = $request->token;
 
-        $allowedHost = "manage.eageskool.com";
-       // dd($allowedHost);
+        $allowedHost = parse_url("https://manage.eageskool.com", PHP_URL_HOST);
         $referer = $request->headers->get('referer');
-        dd($referer);
         // dd(parse_url($referer, PHP_URL_HOST));
         if (parse_url($referer, PHP_URL_HOST) != $allowedHost) {
             dd(parse_url($referer, PHP_URL_HOST), $allowedHost);
