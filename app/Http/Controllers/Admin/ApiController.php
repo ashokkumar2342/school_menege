6277179623f8f6d1f4a0f783d23c4b6a8a941fd7
ashokkumar2342 <ashokkumar2342@gmail.com>
2025-08-27 18:09:20 +0530

    Update ApiController.php

--- app/Http/Controllers/Admin/ApiController.php
+++ app/Http/Controllers/Admin/ApiController.php
@@ -18,6 +18,129 @@ class ApiController extends Controller
 {
     protected $e_controller = "ApiController";
 
+    public function getclass()
+    {
+       try {
+            $classes = MyFuncs::getClasses();
+            return response()->json($classes);
+            
+        } catch (Exception $e) {
+            $e_method = "getclass";
+            return MyFuncs::Exception_error_handler($this->e_controller, $e_method, $e->getMessage());
+        }
+    }
+
+    public function getallsubjects()
+    {
+       try {
+            $rs_subjects = MyFuncs::getSubjectType();
+            return response()->json($rs_subjects);
+            
+        } catch (Exception $e) {
+            $e_method = "getallsubjects";
+            return MyFuncs::Exception_error_handler($this->e_controller, $e_method, $e->getMessage());
+        }
+    }
+
+    public function getsubject($class_id)
+    {
+       try {
+            $rs_subjects = DB::select(DB::raw("SELECT `sbt`.`id` as `opt_id`, concat(`sbt`.`name`, ' - ', case `sub`.`isoptional_id` when 1 then 'Compulsory' else 'Optional' end) as `opt_text` from `subjects` `sub` inner join `subject_types` `sbt` on `sbt`.`id` = `sub`.`subjectType_id` where `sub`.`classType_id` = $class_id and `sub`.`status` = 1 order by `sbt`.`sorting_order_id`;"));
+            return response()->json($rs_subjects);
+            
+        } catch (Exception $e) {
+            $e_method = "getsubject";
+            return MyFuncs::Exception_error_handler($this->e_controller, $e_method, $e->getMessage());
+        }
+    }
+
+    public function getchapter($class_id, $subject_id)
+    {
+       try {
+            $rs_chapter = DB::select(DB::raw("SELECT `id` as `opt_id`, `chapter_topic_name` as `opt_text` from `chapter_topic` where `classType_id` = $class_id and `subjectType_id` = $subject_id;"));
+            return response()->json($rs_chapter);
+            
+        } catch (Exception $e) {
+            $e_method = "getchapter";
+            return MyFuncs::Exception_error_handler($this->e_controller, $e_method, $e->getMessage());
+        }
+    }
+
+    public function getvideo($chapter_id)
+    {
+       try {
+            $videos = DB::select(DB::raw("SELECT * FROM `videos` where `chapter_id` = $chapter_id;"));
+            return response()->json($videos);
+        } catch (Exception $e) {
+            $e_method = "getvideo";
+            return MyFuncs::Exception_error_handler($this->e_controller, $e_method, $e->getMessage());
+        }
+    }
+
+    
+    // Securely stream the video file
+    public function strem_video(Request $request)
+    {
+        try {
+            $rec_id = Crypt::decrypt($request->id);
+            $token = $request->token;
+
+            // $allowedHost = parse_url('https://manage.eageskool.com', PHP_URL_HOST);
+            $referer = $request->headers->get('referer');
+            $allowedHosts = ['manage.eageskool.com', '127.0.0.1', 'localhost', 'demo.eageskool.com', 'eduvia.eageskool.com'];
+            if (!in_array(parse_url($referer, PHP_URL_HOST), $allowedHosts)) {
+                return view('error.home');
+            }
+
+            $videos = DB::select(DB::raw("SELECT * FROM `videos` where `id` = $rec_id;"));
+            $url = $videos[0]->video_path;
+            $storagePath='https://eageskoolvideo.s3.ap-south-1.amazonaws.com/'.$url;
+            return redirect()->away($storagePath);
+        } catch (Exception $e) {
+            Log::error('UserManualController-videoStream: ' . $e->getMessage());
+            return view('error.home');
+        }
+    }
+
+    public function getpdf($chapter_id)
+    {
+       try {
+            $pdfs = DB::select(DB::raw("SELECT * FROM `pdfs` where `chapter_id` = $chapter_id;"));
+            return response()->json($pdfs);
+            
+        } catch (Exception $e) {
+            $e_method = "getpdf";
+            return MyFuncs::Exception_error_handler($this->e_controller, $e_method, $e->getMessage());
+        }
+    }
+
+    // public function securePdfView(Request $request, $encryptedPath)
+    // {
+    //     try {
+    //         $referer = $request->headers->get('referer');
+    //         $allowedHosts = ['manage.eageskool.com', '127.0.0.1', 'localhost'];
+
+    //         if (!$referer || !in_array(parse_url($referer, PHP_URL_HOST), $allowedHosts)) {
+    //             abort(403, 'Unauthorized access.');
+    //         }
+
+    //         $relativePath = Crypt::decrypt($encryptedPath);
+    //         $fullPath = storage_path($relativePath);
+
+    //         if (!\File::exists($fullPath)) {
+    //             abort(404, 'PDF not found');
+    //         }
+
+    //         return response()->file($fullPath, [
+    //             'Content-Type' => 'application/pdf',
+    //             'Content-Disposition' => 'inline; filename="secure.pdf"',
+    //         ]);
+    //     } catch (\Exception $e) {
+    //         \Log::error('securePdfView failed: ' . $e->getMessage());
+    //         return abort(403, 'Invalid or expired link');
+    //     }
+    // }
+
     public function show($enc, Request $request)
     {
         // 1) Decrypt S3 key/path
@@ -126,129 +249,6 @@ class ApiController extends Controller
                 fclose($stream);
             }
         }, 200, $headers);
-    }
-
-    public function getclass()
-    {
-       try {
-            $classes = MyFuncs::getClasses();
-            return response()->json($classes);
-            
-        } catch (Exception $e) {
-            $e_method = "getclass";
-            return MyFuncs::Exception_error_handler($this->e_controller, $e_method, $e->getMessage());
-        }
-    }
-
-    public function getallsubjects()
-    {
-       try {
-            $rs_subjects = MyFuncs::getSubjectType();
-            return response()->json($rs_subjects);
-            
-        } catch (Exception $e) {
-            $e_method = "getallsubjects";
-            return MyFuncs::Exception_error_handler($this->e_controller, $e_method, $e->getMessage());
-        }
-    }
-
-    public function getsubject($class_id)
-    {
-       try {
-            $rs_subjects = DB::select(DB::raw("SELECT `sbt`.`id` as `opt_id`, concat(`sbt`.`name`, ' - ', case `sub`.`isoptional_id` when 1 then 'Compulsory' else 'Optional' end) as `opt_text` from `subjects` `sub` inner join `subject_types` `sbt` on `sbt`.`id` = `sub`.`subjectType_id` where `sub`.`classType_id` = $class_id and `sub`.`status` = 1 order by `sbt`.`sorting_order_id`;"));
-            return response()->json($rs_subjects);
-            
-        } catch (Exception $e) {
-            $e_method = "getsubject";
-            return MyFuncs::Exception_error_handler($this->e_controller, $e_method, $e->getMessage());
-        }
-    }
-
-    public function getchapter($class_id, $subject_id)
-    {
-       try {
-            $rs_chapter = DB::select(DB::raw("SELECT `id` as `opt_id`, `chapter_topic_name` as `opt_text` from `chapter_topic` where `classType_id` = $class_id and `subjectType_id` = $subject_id;"));
-            return response()->json($rs_chapter);
-            
-        } catch (Exception $e) {
-            $e_method = "getchapter";
-            return MyFuncs::Exception_error_handler($this->e_controller, $e_method, $e->getMessage());
-        }
-    }
-
-    public function getvideo($chapter_id)
-    {
-       try {
-            $videos = DB::select(DB::raw("SELECT * FROM `videos` where `chapter_id` = $chapter_id;"));
-            return response()->json($videos);
-        } catch (Exception $e) {
-            $e_method = "getvideo";
-            return MyFuncs::Exception_error_handler($this->e_controller, $e_method, $e->getMessage());
-        }
-    }
-
-    
-    // Securely stream the video file
-    public function strem_video(Request $request)
-    {
-        try {
-            $rec_id = Crypt::decrypt($request->id);
-            $token = $request->token;
-
-            // $allowedHost = parse_url('https://manage.eageskool.com', PHP_URL_HOST);
-            $referer = $request->headers->get('referer');
-            $allowedHosts = ['manage.eageskool.com', '127.0.0.1', 'localhost', 'demo.eageskool.com', 'eduvia.eageskool.com'];
-            if (!in_array(parse_url($referer, PHP_URL_HOST), $allowedHosts)) {
-                return view('error.home');
-            }
-
-            $videos = DB::select(DB::raw("SELECT * FROM `videos` where `id` = $rec_id;"));
-            $url = $videos[0]->video_path;
-            $storagePath='https://eageskoolvideo.s3.ap-south-1.amazonaws.com/'.$url;
-            return redirect()->away($storagePath);
-        } catch (Exception $e) {
-            Log::error('UserManualController-videoStream: ' . $e->getMessage());
-            return view('error.home');
-        }
-    }
-
-    public function getpdf($chapter_id)
-    {
-       try {
-            $pdfs = DB::select(DB::raw("SELECT * FROM `pdfs` where `chapter_id` = $chapter_id;"));
-            return response()->json($pdfs);
-            
-        } catch (Exception $e) {
-            $e_method = "getpdf";
-            return MyFuncs::Exception_error_handler($this->e_controller, $e_method, $e->getMessage());
-        }
-    }
-
-    public function securePdfView(Request $request, $encryptedPath)
-    {
-        try {
-            $referer = $request->headers->get('referer');
-            $allowedHosts = ['manage.eageskool.com', '127.0.0.1', 'localhost'];
-
-            if (!$referer || !in_array(parse_url($referer, PHP_URL_HOST), $allowedHosts)) {
-                abort(403, 'Unauthorized access.');
-            }
-
-            $relativePath = Crypt::decrypt($encryptedPath);
-            $fullPath = storage_path($relativePath);
-
-            if (!\File::exists($fullPath)) {
-                abort(404, 'PDF not found');
-            }
-
-            return response()->file($fullPath, [
-                'Content-Type' => 'application/pdf',
-                'Content-Disposition' => 'inline; filename="secure.pdf"',
-            ]);
-        } catch (\Exception $e) {
-            \Log::error('securePdfView failed: ' . $e->getMessage());
-            return abort(403, 'Invalid or expired link');
-        }
     }
 
 }
